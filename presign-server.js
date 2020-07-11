38810d98bf67050ba6d4ec095abc1a237116ad50
Harshavardhana <harsha@minio.io>
2020-07-11 16:26:29 -0700

    upgrade to minio 7.0.x

--- presign-server.js
+++ presign-server.js
@@ -1,18 +1,18 @@
 var Minio = require('minio');
 
 var client = new Minio.Client({
-    endPoint: 'play.minio.io',
+    endPoint: 'localhost',
     port: 9000,
-    useSSL: true,
-    accessKey: 'Q3AM3UQ867SPQQA43P2F',
-    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+    useSSL: false,
+    accessKey: 'minio',
+    secretKey: 'minio123',
 })
 
 // express is a small HTTP server wrapper, but this works with any HTTP server
 const server = require('express')()
 
 server.get('/presignedUrl', (req, res) => {
-    client.presignedPutObject('uploads', req.query.name, (err, url) => {
+    client.presignedPutObject('testbucket', req.query.name, (err, url) => {
         if (err) throw err
         res.end(url)
     })
